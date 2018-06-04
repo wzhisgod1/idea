fd59ff05f5c58044b6b8142f96293b7b6f19f9f6
wzh <1009070047@qq.com>
2018-06-04 14:02:57 +0800

    Initial commit

---
+++ springboot1/src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by admins on 2018/6/3.
+ */
+@RestController
+public class HelloWorldController {
+    @RequestMapping("/hello")
+    public String index() {
+        return "Hello World";
+    }
+
+}
