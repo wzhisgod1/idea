a45855e8d35d33368b2d8df429c58abed4aac90a
wzh <1009070047@qq.com>
2018-06-06 11:08:58 +0800

    1

--- springboot1/src/main/java/com/example/demo/controller/HelloWorldController.java
+++ springboot1/src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @RequestMapping("/hello")
     public String index() {
-        return "Hello World";
+        return "Hello World123";
     }
 
 }
